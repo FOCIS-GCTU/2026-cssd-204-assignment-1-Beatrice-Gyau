b4472ae83a2a958f9cd686d5292f83f3f80a05d7
Beatrice-Gyau <2425402414@live.gctu.edu.gh>
2026-05-30 18:58:18 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+## File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Gyau Beatrice
+# STUDENT ID: 2425402414
+# Email: 2425402414@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Gyau Beatrice , this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in large letters
 
+    print()
+    print("...MGB")
+    print()
+
+    print("...MMMM....MMMM.........GGGGGGGGGGG........BBBBBBBBBB.......")
+    print("...MMMM....MMMM........GG..................BB........BB.....")
+    print("...MM.MM..MM.MM........GG..................BB........BB.....")
+    print("...MM.MM..MM.MM........GG.......... .......BB........BB.....")
+    print("...MM.MM..MM.MM........GG......GGGG........BBBBBBBBBB.......")
+    print("...MM..MMMM..MM........GG........GG........BB........BB.....")
+    print("...MM..MMMM..MM........GG........GG........BB........BB.....")
+    print("...MM...MM...MM........GG........GG........BB........BB.....")
+    print("...MM...MM...MM...*...GG........GG......BB........BB...*")
+    print("...MM...MM...MM...*....GGGGGGGGGG.......BBBBBBBBBB.....*")
+
+    print()
 
 
 main()
